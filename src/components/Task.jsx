8c21db632b9934ea783034078a361b0034297326
waleery <walenda1997@gmail.com>
2023-12-24 23:58:48 +0100

    Added function to delete task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,25 @@
 import { useStore } from "../store";
 import "./Task.css";
 import classNames from "classnames";
+import trashIcon from "./../assets/tash-icon.svg";
 
 const Task = ({ title }) => {
-    const task = useStore((store) => store.tasks.find((task) => task.title === title))
+    const task = useStore((store) =>
+        store.tasks.find((task) => task.title === title)
+    );
+
+    const deleteTask = useStore((store) => store.deleteTask) 
 
     return (
         <div className="task">
             <div>{task.title}</div>
             <div className="bottomWrapper">
-                <div></div>
-                <div className={classNames("status", task.state)}>{task.state}</div>
+                <div className="deleteIcon">
+                    <img src={trashIcon} alt="delete-icon" onClick={() =>deleteTask(task.title)}/>
+                </div>
+                <div className={classNames("status", task.state)}>
+                    {task.state}
+                </div>
             </div>
         </div>
     );
--- src/store.js
+++ src/store.js
@@ -8,9 +8,13 @@ const store = (set) => ({
     ],
     addTask: (title, state) =>
         set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+    deleteTask: (title) =>
+        set((store) => ({
+            tasks: store.tasks.filter((task) => task.title !== title),
+        })),
 });
 
-// https://github.com/pmndrs/zustand/discussions/1937 
+// https://github.com/pmndrs/zustand/discussions/1937
 export const useStore = createWithEqualityFn(store, shallow);
 
 //When we need custom compare function then second argument is 'Object.is'
