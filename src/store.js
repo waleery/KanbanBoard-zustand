889d5e7fcd70c62bbd97c5b322b89606b788165c
waleery <walenda1997@gmail.com>
2023-12-28 14:25:50 +0100

    Removed default tasks

--- src/store.js
+++ src/store.js
@@ -1,36 +1,48 @@
 import { shallow } from "zustand/shallow";
 import { createWithEqualityFn } from "zustand/traditional";
-import {devtools, persist} from 'zustand/middleware'
+import { devtools, persist } from "zustand/middleware";
 
 const store = (set) => ({
-    tasks: [
-        { title: "TestTask", state: "PLANNED" },
-        { title: "TestTask2", state: "PLANNED" },
-    ],
+    tasks: [],
     draggedTask: null,
     addTask: (title, state) =>
         // false/true tells zustand to just manipulate or replace object in store
-        set((store) => ({ tasks: [...store.tasks, { title, state }] }), false, "addTask"),
+        set(
+            (store) => ({ tasks: [...store.tasks, { title, state }] }),
+            false,
+            "addTask"
+        ),
     deleteTask: (title) =>
-        set((store) => ({
-            tasks: store.tasks.filter((task) => task.title !== title),
-        }), false, "deleteTask"),
-    setDraggedTask: (title) => set({ draggedTask: title }, false, "setDraggedTask"),
-    moveTask: (title, state) =>
-        set((store) => ({
-            tasks: store.tasks.map((task) => {
-                if (task.title === title) {
-                    return { title, state };
-                } else {
-                    return task;
-                }
+        set(
+            (store) => ({
+                tasks: store.tasks.filter((task) => task.title !== title),
             }),
-        }), false, "moveTask"),
+            false,
+            "deleteTask"
+        ),
+    setDraggedTask: (title) =>
+        set({ draggedTask: title }, false, "setDraggedTask"),
+    moveTask: (title, state) =>
+        set(
+            (store) => ({
+                tasks: store.tasks.map((task) => {
+                    if (task.title === title) {
+                        return { title, state };
+                    } else {
+                        return task;
+                    }
+                }),
+            }),
+            false,
+            "moveTask"
+        ),
 });
-console.log(store)
+console.log(store);
 
 // https://github.com/pmndrs/zustand/discussions/1937
-export const useStore = createWithEqualityFn(persist(devtools(store, shallow), {name:"store"}));
+export const useStore = createWithEqualityFn(
+    persist(devtools(store, shallow), { name: "store" })
+);
 
 //When we need custom compare function then second argument is 'Object.is'
 //export const useStore = createWithEqualityFn(store, Object.is);
