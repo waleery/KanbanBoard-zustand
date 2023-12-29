fd4643445c57ed3c993571c3c81afcd90b765810
waleery <walenda1997@gmail.com>
2023-12-29 19:29:03 +0100

    added test to check the correctness of adding tasks

--- src/store.test.jsx
+++ src/store.test.jsx
@@ -22,3 +22,20 @@ test('should return default value (empty array) at the start', () => {
     //Checking with how many arguments the function 'effect' was executed
     expect(effect).toHaveBeenCalledWith([])
 })
+
+test('should add ann items to the store and rerun the effect', () => {
+    const selector = (store) =>({tasks:store.tasks, addTask: store.addTask})
+    const effect = vi.fn().mockImplementation((items) => {
+        if(items.tasks.length === 0){
+            items.addTask('a','b')
+        }
+    })
+
+    render(<TestComponent selector={selector} effect={effect} />)
+
+    //Checking with how many arguments the function 'effect' was executed
+    expect(effect).toHaveBeenCalledTimes(2)
+
+    //Checking with what argument function 'effect' was executed
+    expect(effect).toHaveBeenCalledWith(expect.objectContaining({tasks:[{title:'a', state:'b'}]}))
+})
